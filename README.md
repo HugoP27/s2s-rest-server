8e205d86393d7225fdb6e86694f9bf0a73f59af3
Hugo <hugopaul@live.com>
2023-10-23 23:50:07 +0200

    exercise completed

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# s2s-rest-server
-Demo web server for Stock2Shop interview
+# Stock2Shop RESTful server demo
+
+S2S Interview REST server.
+
+Below is an explanation of the technologies used.
+
+## Technologies
+
+### core
+
+- Node.js
+
+### libraries
+
+- express.js: easy to use framework for node that makes creating routes easy, simple and maintainable
+- SQLite3: as test database for api
+- body-parser: parses json data in endpoints on the fly
+- nodemon: easy to use node development server
+
+## why node
+
+- I am comfortable using javascript and node
+- Node is non blocking and fast (can handle a large number of concurrent requests)
+- large ecosystem and easy to learn
+- Node supports middleware which makes it cleaner and easier to use
+- Easy to scale as you can just add more instances to your existing server
+- various tools available for performance monitoring
+
+## how to build
+
+- Clone repo
+- open a cmd in the root folder
+- run the command "npm install"
+- run "npm start"
+
+## using the api
+
+- make sure the server is running by checking the terminal opened in the previous step
+- go to localhost:3000/v1/products?sku=1 (feel free to change the ID, values between 1 and 10 will work)
+- use postman or similar tool to create a post request, similar to the above but an sku is optional.
--- server.js
+++ server.js
@@ -9,22 +9,21 @@ const db = new sqlite3.Database("products");
 
 // Initialize the database table if it doesn't exist
 db.serialize(() => {
-  db.run(
-    "CREATE TABLE IF NOT EXISTS products (sku INTEGER PRIMARY KEY, attributes TEXT)"
-  );
+  db.run("DROP TABLE IF EXISTS products");
+  db.run("CREATE TABLE products (sku SERIAL PRIMARY KEY, attributes TEXT)");
 
   // Sample data to insert
   const sampleData = [
-    { sku: 1, attributes: '{"size":"small","grams":"100","name":"mouse"}' },
-    { sku: 2, attributes: '{"size":"small","grams":"100","name":"mouse"}' },
-    { sku: 3, attributes: '{"size":"small","grams":"100","name":"mouse"}' },
-    { sku: 4, attributes: '{"size":"small","grams":"100","name":"mouse"}' },
-    { sku: 5, attributes: '{"size":"small","grams":"100","name":"mouse"}' },
-    { sku: 6, attributes: '{"size":"small","grams":"100","name":"mouse"}' },
-    { sku: 7, attributes: '{"size":"small","grams":"100","name":"mouse"}' },
-    { sku: 8, attributes: '{"size":"small","grams":"100","name":"mouse"}' },
-    { sku: 9, attributes: '{"size":"small","grams":"100","name":"mouse"}' },
-    { sku: 10, attributes: '{"size":"small","grams":"100","name":"mouse"}' },
+    { sku: 1, attributes: '{size:"small",grams:40,name:"bag"}' },
+    { sku: 2, attributes: '{size:"large",grams:1500,name:"fanta"}' },
+    { sku: 3, attributes: '{size:"large",grams:2000,name:"cola"}' },
+    { sku: 4, attributes: '{size:"medium",grams:750,name:"flower"}' },
+    { sku: 5, attributes: '{size:"small",grams:250,name:"macaroni"}' },
+    { sku: 6, attributes: '{size:"large",grams:1200,name:"cheese"}' },
+    { sku: 7, attributes: '{size:"medium",grams:600,name:"t-bone steak"}' },
+    { sku: 8, attributes: '{size:"small",grams:80,name:"simba tomato"}' },
+    { sku: 9, attributes: '{size:"very large",grams:5000,name:"weight"}' },
+    { sku: 10, attributes: '{size:"small",grams:150,name:"box"}' },
   ];
 
   // Insert the sample data into the table
@@ -40,12 +39,12 @@ db.serialize(() => {
 // Middleware for parsing JSON data in the request body
 app.use(bodyParser.json());
 
-// HTML page to show when the user visits root localhost:3000
+// HTML page to show when the user visits root (localhost:3000)
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
-// Get product by SKU
+// Endpoint to get product by SKU
 app.get("/v1/products", (req, res) => {
   const sku = req.query.sku;
 
@@ -74,26 +73,40 @@ app.get("/v1/products", (req, res) => {
   });
 });
 
-// Endpoint for creating a new product.
+// Endpoint for creating a new product or updating an existing one.
 app.post("/v1/products", (req, res) => {
   const { sku, attributes } = req.body;
 
-  if (sku === undefined || typeof attributes !== "string") {
+  if (typeof attributes !== "string") {
     return res.status(400).json({ error: "Invalid request data." });
   }
 
-  // Insert the new product into the database
-  db.run(
-    "INSERT INTO products (sku, attributes) VALUES (?, ?)",
-    sku,
-    attributes,
-    (err) => {
-      if (err) {
-        return res.status(500).json({ error: err.message });
-      }
-      res.status(201).json({ message: "Product added successfully" });
+  let sql;
+  let params;
+
+  if (sku === undefined) {
+    // If SKU is not provided, use an INSERT statement.
+    sql = "INSERT INTO products (attributes) VALUES (?)";
+    params = [attributes];
+  } else {
+    // If SKU is provided, use an UPDATE statement.
+    sql = "UPDATE products SET attributes = ? WHERE sku = ?";
+    params = [attributes, sku];
+  }
+
+  // Execute the SQL statement.
+  db.run(sql, params, function (err) {
+    if (err) {
+      return res.status(500).json({ error: err.message });
     }
-  );
+
+    if (this.changes === 0) {
+      // If no rows were affected, it means no existing product was updated.
+      res.status(201).json({ message: "Product added successfully" });
+    } else {
+      res.status(200).json({ message: "Product updated successfully" });
+    }
+  });
 });
 
 app.listen(port, () => {
